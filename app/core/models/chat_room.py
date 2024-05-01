3b8e432c8a4a0f6e7f0fb6d4a7d9ca878361c87a
JooHee Gwon <joohee06197@gmail.com>
2024-05-02 00:23:11 +0900

    인덱스 방식 변경

--- app/core/models/chat_room.py
+++ app/core/models/chat_room.py
@@ -3,12 +3,6 @@ from tortoise import Model, fields
 
 class ChatRoom(Model):
     id = fields.BigIntField(pk=True)
-    user = fields.ForeignKeyField('models.User', related_name='users', db_constraint=False)
+    user = fields.ForeignKeyField('models.User', related_name='users', db_constraint=False, index=True)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
-
-    class Meta:
-        table = "chat_rooms"
-        index = [
-            "user_id",
-        ]
--- app/core/models/message.py
+++ app/core/models/message.py
@@ -3,7 +3,7 @@ from tortoise import Model, fields
 
 class Message(Model):
     id = fields.BigIntField(pk=True)
-    chat_room = fields.ForeignKeyField('models.ChatRoom', related_name='chat_room')
+    chat_room = fields.ForeignKeyField('models.ChatRoom', related_name='chat_room', index=True)
     sender = fields.ForeignKeyField('models.User', related_name='sender')
     content = fields.TextField()
     created_at = fields.DatetimeField(auto_now_add=True)
